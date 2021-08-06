1de201b59be3cd774617dd8e87b35165840c38e2
Michaela <malitherland@gmail.com>
2021-08-05 20:05:54 -0400

    created HTML DOM and index page for website

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const webpage = document.getElementById(\"content\"); \r\n\r\ndocument.body.style.backgroundImage= \"url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F9%2F91%2FTom%27s_Restaurant%252C_Manhattan.jpg&f=1&nofb=1')\";\r\n\r\nconst textArea = document.createElement(\"DIV\");\r\ntextArea.setAttribute(\"id\", \"for-text\");\r\n\r\ntextArea.style.display= \"flex\";\r\ntextArea.style.justifyContent= \"center\";\r\ntextArea.style.padding= \"20px\";\r\n\r\ntextArea.style.margin = \"5px\";\r\ntextArea.style.fontFamily= \"monospace\";\r\n\r\n\r\nconst restaurantTitling = document.createElement(\"h1\");\r\nrestaurantTitling.style.color= \"rgb(255,250,240)\";\r\nrestaurantTitling.style.padding= \"25px\";\r\nrestaurantTitling.style.borderRadius= \"25px\";\r\n\r\nrestaurantTitling.style.textAlign= \"Center\";\r\n\r\nrestaurantTitling.textContent= \"Tom's Restaurant\";\r\n\r\nconst restaurantSubTitling= document.createElement(\"h2\");\r\nrestaurantSubTitling.style.color= \"rgb(255,250,240)\";\r\nrestaurantSubTitling.style.padding= \"30px\";\r\nrestaurantSubTitling.style.textAlign= \"center\";\r\nrestaurantSubTitling.style.borderRadius= \"25px\";\r\nrestaurantSubTitling.textContent = \"About\";\r\nrestaurantSubTitling.style.marginRight= \"30px\";\r\nrestaurantSubTitling.style.marginLeft= \"30%\";\r\n\r\nconst restaurantSubTitling1= document.createElement(\"h2\");\r\nrestaurantSubTitling1.style.color= \"rgb(255,250,240)\";\r\nrestaurantSubTitling1.style.padding= \"30px\";\r\nrestaurantSubTitling1.style.marginRight= \"30px\";\r\nrestaurantSubTitling1.style.textAlign= \"center\";\r\nrestaurantSubTitling1.textContent = \"Menu\";\r\nrestaurantSubTitling1.style.marginRight= \"30px\";\r\n\r\nconst restaurantSubTitling2= document.createElement(\"h2\");\r\nrestaurantSubTitling2.style.color= \"rgb(255,250,240)\";\r\nrestaurantSubTitling2.style.padding= \"30px\";\r\nrestaurantSubTitling2.style.textAlign= \"center\";\r\nrestaurantSubTitling2.textContent = \"Contact Us\";\r\nrestaurantSubTitling2.style.marginRight= \"30px\";\r\n\r\n\r\n\r\nconst blurb = document.createElement(\"DIV\");\r\n\r\nconst blurb_text = document.createElement(\"p\");\r\nblurb_text.textContent = \"Family owned and run, we at Tom's Restaurant remain the old-fashioned diner style restaurant to this day.\"\r\n\r\n\r\ntextArea.appendChild(restaurantTitling);\r\ntextArea.appendChild(restaurantSubTitling);\r\n\r\ntextArea.appendChild(restaurantSubTitling1);\r\ntextArea.appendChild(restaurantSubTitling2);\r\n\r\n\r\nwebpage.appendChild(textArea);\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
